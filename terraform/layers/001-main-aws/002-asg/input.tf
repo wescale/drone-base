252f1dd2309e8ee2873ad088750fc039b4abbd94
Jérôme Devoucoux <jdevoucoux@gmail.com>
2019-09-10 10:54:03 +0200

    userdata

--- terraform/layers/001-main-aws/002-asg/input.tf
+++ terraform/layers/001-main-aws/002-asg/input.tf
@@ -25,6 +25,46 @@ data "aws_ami" "debian" {
   owners = ["379101102735"] # Debian Project
 }
 
+data "template_file" "user-data" {
+  template = <<EOF
+#cloud-config
+packages:
+  - vim
+  - git
+  - curl
+  - htop
+  - ncdu
+runcmd:
+  - 'apt update'
+  - 'apt upgrade -y'
+  - 'apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common'
+  - 'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -'
+  - 'apt-key fingerprint 0EBFCD88'
+  - 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'
+  - 'apt update'
+  - 'apt install -y docker-ce docker-ce-cli containerd.io'
+  - 'docker run \
+      --volume=/var/run/docker.sock:/var/run/docker.sock \
+      --volume=/var/lib/drone:/data \
+      --env=DRONE_GITHUB_SERVER=https://github.com \
+      --env=DRONE_GITHUB_CLIENT_ID=${var.drone.github_id} \
+      --env=DRONE_GITHUB_CLIENT_SECRET=${var.drone.github_secret} \
+      --env=DRONE_USER_CREATE=username:${var.drone.username},admin:true \
+      --env=DRONE_RUNNER_CAPACITY=2 \
+      --env=DRONE_SERVER_HOST=${var.drone.host} \
+      --env=DRONE_SERVER_PROTO=https \
+      --env=DRONE_TLS_AUTOCERT=true \
+      --publish=80:80 \
+      --publish=443:443 \
+      --restart=always \
+      --detach=true \
+      --name=drone \
+      drone/drone:1'
+EOF
+}
+# # install k3s
+# - 'curl -sfL https://get.k3s.io | sh -'
+
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
